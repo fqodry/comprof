2cbcc1103ef7292eee132ef35caaae71427610b8
Moh. Firman Qodry <firman.qodry@gmail.com>
2018-02-21 17:06:25 +0700

    update homepage

--- application/views/home/homepage.php
+++ application/views/home/homepage.php
@@ -7,13 +7,13 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block w-100" src="assets/img/landing/banner-kaos3.jpg" alt="First slide">
+          <img class="d-block w-100" src="assets/img/landing/banner-02.jpg" alt="First slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="assets/img/landing/banner-mug2.jpg" alt="Second slide">
+          <img class="d-block w-100" src="assets/img/landing/banner-01.jpg" alt="Second slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block w-100" src="assets/img/landing/banner-pillow2.jpg" alt="Third slide">
+          <img class="d-block w-100" src="assets/img/landing/banner-03.jpg" alt="Third slide">
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -25,6 +25,69 @@
         <span class="sr-only">Next</span>
       </a>
     </div>
+
+    <!-- Value Qoddeclabs -->
+    <section class="features-icons bg-light text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-tag m-auto text-primary"></i>
+              </div>
+              <h3>Competitive Price</h3>
+              <p class="lead mb-0">We're not build a bad product with cheap price. But we are realistic about quality by giving proper price.</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-heart m-auto text-primary"></i>
+              </div>
+              <h3>Build With Heart</h3>
+              <p class="lead mb-0">Every product we have are build fully from our heart. So don't hesitate about our product.</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-cup m-auto text-primary"></i>
+              </div>
+              <h3>Clean Code</h3>
+              <p class="lead mb-0">We practice a clean code for every product we build. A good website build from a clean code.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Content -->
+    <section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/landing/curious-girl-2.jpg');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>What Is Qoddeclabs?</h2>
+            <p class="lead mb-0">Qoddeclabs is an IT Consultant and specialize in building website application such as E-Commerce, Company Profile, HRIS, ERP, and many more. This year (2018) we opened a request for building mobile application. We love to help people solve their problem :)</p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/landing/customers-2.jpg');"></div>
+          <div class="col-lg-6 my-auto showcase-text">
+            <h2>Who Our Customer?</h2>
+            <p class="lead mb-0">You are our customer! Yes, all those who want to solve their problem about building website or apps. We're ready to help you.</p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/landing/friendship.jpg');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>What makes you love us?</h2>
+            <p class="lead mb-0">We are trusted website builder platform. We build all of our products with love. We turn our fatigue into a cool products. And also we practice a clean code to build our products.</p>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <!-- Testimonials -->
     <section class="testimonials text-center bg-light">
@@ -56,76 +119,13 @@
       </div>
     </section>
 
-    <!-- Content -->
-    <section class="showcase">
-      <div class="container-fluid p-0">
-        <div class="row no-gutters">
-
-          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/landing/curious-girl-2.jpg');"></div>
-          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-            <h2>What Is Rinci?</h2>
-            <p class="lead mb-0">Rinci is a website where you can design your own T-Shirt, Sweater, Jacket, Mug, Pillow and many more. All design will become yours, we are only platform to help your creativity channeled. Let's design with Rinci!</p>
-          </div>
-        </div>
-        <div class="row no-gutters">
-          <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/landing/customers-2.jpg');"></div>
-          <div class="col-lg-6 my-auto showcase-text">
-            <h2>Who Our Customer?</h2>
-            <p class="lead mb-0">You are our customer! Yes, all those who want to channel their ideas into a great distro products. T-Shirt, Sweater, Jacket, Hoodie, and even cute mugs and pillow you named it.</p>
-          </div>
-        </div>
-        <div class="row no-gutters">
-          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/landing/friendship.jpg');"></div>
-          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-            <h2>What makes you love rinci?</h2>
-            <p class="lead mb-0">We are trusted custom design platform. We build all of our products with love. We turn our fatigue into a cool products. We guarantee you with 100% money back if you receive a failed products. And last, you are safe to shopping with us because we have positively charming peoples and powerfull security systems.</p>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- Value Rinci -->
-    <section class="features-icons bg-light text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-              <div class="features-icons-icon d-flex">
-                <i class="icon-wallet m-auto text-primary"></i>
-              </div>
-              <h3>100% Guarantee</h3>
-              <p class="lead mb-0">100% money back guarantee if you receive our product in bad condition or not worth using.</p>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-              <div class="features-icons-icon d-flex">
-                <i class="icon-handbag m-auto text-primary"></i>
-              </div>
-              <h3>Order Single Custom</h3>
-              <p class="lead mb-0">With Rinci you can order our custom products in single unit. Yes, you can order one designed items!</p>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-              <div class="features-icons-icon d-flex">
-                <i class="icon-shield m-auto text-primary"></i>
-              </div>
-              <h3>Safe Shopping</h3>
-              <p class="lead mb-0">Rinci is a trusty website which has powerfull security system.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
     <!-- Call to Action -->
     <section class="call-to-action text-white text-center">
       <div class="overlay"></div>
       <div class="container">
         <div class="row">
           <div class="col-xl-9 mx-auto">
-            <h2 class="mb-4">Ready to get started? Sign up now!</h2>
+            <h2 class="mb-4">Ready to get started?</h2>
           </div>
           <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
             <form>
